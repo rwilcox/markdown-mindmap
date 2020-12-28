65fa708d74f7c0e3e64d3c157d32870a480b0b11
Ryan Wilcox <rwilcox@wilcoxd.com>
2020-12-27 23:23:11 -0500

    App.js correctly typechecking

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import type { Ref, Node } from 'react'
 import SafeGraphvizRender from './SafeGraphvizRender'
 import { Graphviz } from 'graphviz-react'
 
@@ -9,35 +10,42 @@ type State = {
 }
 
 class App extends Component< {}, State> {
-    state = {
+    state : State = {
         graphvizStr: `graph { "hi" }`
     }
 
-    graphvizTextArea : ?HTMLTextAreaElement
-    safeGraphViz     : ?SafeGraphvizRender
+    graphvizTextArea : { current: null | HTMLTextAreaElement }  // WTF : https://stackoverflow.com/q/50076176/224334
+    safeGraphViz     : { current: null | SafeGraphvizRender }
 
-    constructor(props) {
+    constructor(props: any) {
         super(props)
 
         this.graphvizTextArea = React.createRef()
         this.safeGraphViz     = React.createRef()
+
     }
 
 
-    transferGraphvizTextToState = (evt: SyntheticEvent<HTMLButtonElement>) => {
+    // use this oddball declaration style because I want to preserve this binding
+    // at the point of function creation (vs using _.bind at runtime).
+    // apparently this is the cool new way even though it harshes my vibe.
+    _transferGraphvizTextToState = (evt: SyntheticEvent<HTMLButtonElement>) => {
         if (this.graphvizTextArea !== null) {
-            let dot = this.graphvizTextArea.current.value
+            if (this.graphvizTextArea.current !== null) {
+                let dot = this.graphvizTextArea.current.value
 
-            this.safeGraphViz.current.resetError()
-            this.setState( {graphvizStr: dot})
+                if ( this.safeGraphViz.current ) this.safeGraphViz.current.resetError()
+                this.setState( {graphvizStr: dot})
+            }
         }
     }
+
 
     // <header className="App-header">
     //  <p>Edit <code>src/App.js</code> and save to reload.</p>
     // </header>
- 
-    render() {
+
+    render() : Node {
         return (
             <div className="App">
                 <section>
@@ -45,7 +53,7 @@ class App extends Component< {}, State> {
                         <textarea ref={this.graphvizTextArea}
                                         id="graphviz-input" cols="80" style={{height: "15em"}}
                                         >{this.state.graphvizStr}</textarea>
-                        <button onClick={this.transferGraphvizTextToState}>Render</button>
+                        <button onClick={this._transferGraphvizTextToState}>Render</button>
                     </div>
                     <div>
                         { /* ARGGHH this will STILL show the error overlay in dev mode, even
