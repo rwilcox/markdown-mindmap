a587c1df4de6e66782f3c805c689553b526a0fdb
Ryan Wilcox <rwilcox@wilcoxd.com>
2021-04-10 22:49:46 -0400

    install aws-amplify

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header'
+
+Amplify.configure({
+  Auth: {
+    userPoolId: 'us-east-1_7SwoAY02u', // TODO: replace this with env variable
+    region: 'us-east-1',
+    userPoolWebClientId: '73pp6kcka3e4vt1cnt6plporge' //TODO: replace this with env variable
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <Header />
+        <AmplifyAuthenticator />
+  // <App />
   </React.StrictMode>,
     ( ( document.getElementById('root') : any ): HTMLElement )
     // force bad type checking for ease of development. This potential runtime error is fine
