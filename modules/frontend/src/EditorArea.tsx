5471e9a339f78421eba64db23713328dda85285c
Ryan Wilcox <rwilcox@wilcoxd.com>
2021-08-22 19:14:22 -0400

    use fancy codejar editor and not just plain ol textarea
    improvement: now has LINE NUMBERS!!!!

--- modules/frontend/src/EditorArea.tsx
+++ modules/frontend/src/EditorArea.tsx
@@ -6,31 +6,36 @@ import SafeGraphvizRender from './SafeGraphvizRender'
 import type { RemarkNodeType } from './models/RemarkNodeType'
 import MarkdownNode from './models/MarkdownNode'
 
-const remark = require('remark')
+import { ReactCodeJar } from "react-codejar";
 
+const remark = require('remark')
 type State = {
     graphvizStr : string,
-    graphOrient : string
+    graphOrient : string,
+    markdownStr : string,
+    draftGraphvizStr: string
 }
 
 
 class EditorArea extends Component< {}, State> {
     state : State = {
         graphvizStr: `graph { "hi" }`,
-        graphOrient: "TB"
+        draftGraphvizStr: `graph { "hi" }`,
+        // ^^^ use and manage a "draft" because we don't want React trying to render half completed graphviz and throwing a fit
+        // (which will happen if we try that). Graphviz rendering happens "only" explicitly when the render button(s) are pressed.
+        // WD-rpw 08/22/2021
+
+        graphOrient: "TB",
+        markdownStr: `# hi`
     }
 
-    graphvizTextArea : { current: null | HTMLTextAreaElement }  // WTF : https://stackoverflow.com/q/50076176/224334
     safeGraphViz     : { current: null | SafeGraphvizRender }
-    markdownTextArea : { current: null | HTMLTextAreaElement }
     graphvizDisplayArea : { current: null | HTMLDivElement }
 
     constructor(props: any) {
         super(props)
 
-        this.graphvizTextArea = React.createRef()
         this.safeGraphViz     = React.createRef()
-        this.markdownTextArea = React.createRef()
         this.graphvizDisplayArea = React.createRef()
     }
 
@@ -39,41 +44,29 @@ class EditorArea extends Component< {}, State> {
     // at the point of function creation (vs using _.bind at runtime).
     // apparently this is the cool new way even though it harshes my vibe.
     _transferGraphvizTextToState = (evt:  React.SyntheticEvent) => {
-        if (this.graphvizTextArea !== null) {
-            if (this.graphvizTextArea.current !== null) {
-                let dot = this.graphvizTextArea.current.value
-
-                if ( this.safeGraphViz.current ) this.safeGraphViz.current.resetError()
-                this.setState( {graphvizStr: dot})
-            }
-        }
+        this.setState( { graphvizStr: this.state.draftGraphvizStr} )
+        if ( this.safeGraphViz.current ) this.safeGraphViz.current.resetError()
     }
 
 
     _generateGraphFromMarkdown = (evt: React.SyntheticEvent) => {
-        if ( (this.markdownTextArea !== null) && (this.markdownTextArea.current !== null) ) {
-            let markdownStr = this.markdownTextArea.current.value
+        let markdownStr = this.state.markdownStr
 
-            let markdownAST: RemarkNodeType = remark().parse(markdownStr)
-            let output: MarkdownNode | null = null
-            try {
-                output = MarkdownNode.organizeHeadingEntries(markdownAST.children)
-            } catch (err) {
-                alert("Error when parsing Markdown")   // TODO: import an error console compontent...
-                //console.error(err)
-                return
-            }
-            let generatedGraphStr = output.recursiveGraphvizNode()
-            //console.dir( markdownAST )
-
-            let validgraphViz = `digraph { \n rankdir=${this.state.graphOrient}\n ${generatedGraphStr} \n}`
-            this.setState({graphvizStr: validgraphViz})
-
-            if (this.graphvizTextArea.current) {
-               this.graphvizTextArea.current.value = validgraphViz // TODO: ugh is this right?
-            }
+        let markdownAST: RemarkNodeType = remark().parse(markdownStr)
+        let output: MarkdownNode | null = null
+        try {
+            output = MarkdownNode.organizeHeadingEntries(markdownAST.children)
+        } catch (err) {
+            alert("Error when parsing Markdown")   // TODO: import an error console compontent...
+            //console.error(err)
+            return
         }
+        let generatedGraphStr = output.recursiveGraphvizNode()
+        //console.dir( markdownAST )
 
+        let validgraphViz = `digraph { \n rankdir=${this.state.graphOrient}\n ${generatedGraphStr} \n}`
+        this.setState({graphvizStr: validgraphViz})
+        this.setState({draftGraphvizStr: validgraphViz})
     }
 
 
@@ -99,21 +92,37 @@ class EditorArea extends Component< {}, State> {
 
 
     render() {
+        let editorStyling = {background: "#fdf6e3", color: "#657b83",
+                                                  border: "thin black solid", height: "20em"}
+
+        let markdownTextArea = <ReactCodeJar code={this.state.markdownStr}
+                              lineNumbers={true}
+
+                              highlight={ (x) => {} }
+                              onUpdate={ (x) => { this.setState({markdownStr: x}) } }
+
+                              style={editorStyling}
+                              />
+
+        let graphvizTextArea = <ReactCodeJar code={this.state.draftGraphvizStr}
+                              lineNumbers={true}
+                              highlight={ (x) => {} }
+                              onUpdate={ (x) => {this.setState({draftGraphvizStr: x})} }
+                              style={editorStyling} />
+
         return (
             <div className="App">
 
                 <div style={{height: '20em'}}>
                   <div style={{float: "left", width: '50%'}}>
                     <h1>Markdown</h1>
-                    <textarea ref={this.markdownTextArea} cols={80} style={{height: "15em"}}></textarea>
+                    <div>{markdownTextArea}</div>
                     <button onClick={this._generateGraphFromMarkdown} style={{display: 'block'}}>MMD `&gt;` DOT</button>
                     <input id="graphOrient" type="checkbox" onClick={this._toggleGraphOrient}/><label htmlFor="graphOrient">Left to Right Orientatation</label>
                   </div>
                 <div style={{float: 'right', width: '49%'}}>
                        <h1>Graphviz "dot" language</h1>
-                       <textarea ref={this.graphvizTextArea}
-                                        id="graphviz-input" cols={80} style={{height: "15em"}}
-                                        >{this.state.graphvizStr}</textarea>
+                       <div>{graphvizTextArea}</div>
                        <button onClick={this._transferGraphvizTextToState} style={{display: 'block'}}>Render</button>
                   </div>
                 </div>
